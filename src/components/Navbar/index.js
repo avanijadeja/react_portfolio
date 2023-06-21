867829df109476414437f1d6f8acc3433259075c
Avani <avani.jadeja@gmail.com>
2023-06-21 17:21:29 -0500

    Added logic for navbar in navbar.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,39 @@
 import React, { useEffect } from "react";
 
-function Navbar() {
+function Navbar(props) {
+  const { currentTab, setCurrentTab } = props;
+
   return (
-    <ul>
-      <li>About</li>
-      <li>Work</li>
-      <li>Contact Me</li>
-      <li>Resume</li>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => setCurrentTab("About")}
+          className={currentTab === "About" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => setCurrentTab("contact")}
+          className={currentTab === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => setCurrentTab("Resume")}
+          className={currentTab === "Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </a>
+      </li>
     </ul>
   );
 }

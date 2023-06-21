41b70c0d53d0d9831af2061c2407a6e459eb2b3d
Avani <avani.jadeja@gmail.com>
2023-06-21 17:03:26 -0500

    Add logic to render page in app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "../src/components/Header";
-import Footer from "../src/components/Footer";
 import About from "../src/components/About";
-import Navbar from "../src/components/Navbar";
+import Contact from "../src/components/Contact";
+import Portfolio from "../src/components/Portfolio";
+import Resume from "../src/components/Resume";
+import Footer from "../src/components/Footer";
+import userEvent from "@testing-library/user-event";
 
 function App() {
+
+const [currentTab,setCurrentTab] = userState("About");
+
+const renderTab =() => {
+
+    if(currentTab === "About"){
+        return <About />;
+    }
+    else if( currentTab === "Contact"){
+        return <Contact />;
+    }
+    else if(currentTab === "Portfolio"){
+        return <Portfolio />;
+    }
+    else if(currentTab === "Resume"){
+        return <Resume />;
+    }
+    else{
+        return <About />;
+    }
+};
+
+
 return(
-<Navbar></Navbar>
-);
-
-
-
-
-
+    <>
+    <header></header>
+    <Footer></Footer>
+    </>
+    );
 }
 
 

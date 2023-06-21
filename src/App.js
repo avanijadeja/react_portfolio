727baaddaccb190d6c617a63deec9bbf61606c5f
Avani <avani.jadeja@gmail.com>
2023-06-21 07:34:37 -0500

    Import components folders in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React,{ useState} from 'react';
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import About from "../src/components/About";
+import Navbar from "../src/components/Navbar";
+
 
 function App() {
   return (

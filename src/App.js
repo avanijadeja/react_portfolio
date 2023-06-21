367c6443e497bc05c71cc85e6ff424d7b5a75902
Avani <avani.jadeja@gmail.com>
2023-06-21 09:39:13 -0500

    Added navbar in app.js and index.js in Navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React,{ useState} from 'react';
+import React, { useState } from "react";
+import "./App.css";
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
 import About from "../src/components/About";
 import Navbar from "../src/components/Navbar";
 
-
 function App() {
-  return (
-    <div className="app">
-      Hello
-    </div>
-  );
+return(
+<Navbar></Navbar>
+);
+
+
+
+
+
 }
+
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+function Navbar() {
+  return (
+    <ul>
+      <li>About</li>
+      <li>Work</li>
+      <li>Contact Me</li>
+      <li>Resume</li>
+    </ul>
+  );
+}
+
+export default Navbar;

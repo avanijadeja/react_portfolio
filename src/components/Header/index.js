288b396945848bed88a323f24368eb08f73c7bb2
Avani <avani.jadeja@gmail.com>
2023-06-21 17:38:39 -0500

    solved error in navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,24 @@
 import React from "react";
-import Nav from "../Nav";
+import Navbar from "../Navbar";
 
-function Header(props){
-    const {currentTab, setCurrentTab} = props;
+function Header(props) {
+  const { currentTab, setCurrentTab } = props;
 
-    return(
-        <div>
-            <section>
-                <header className="flex-row px-1">
-                    <div>
-                        <h1>Avani's Portfolio</h1>
-                    </div>
-                    <div>
-                        <Nav currentTab={currentTab} setCurrentTab={setCurrentTab}></Nav>
-                    </div>
-                </header>
-            </section>
-        </div>
-    )
+  return (
+    <div>
+      <section>
+        <header className="flex-row px-1">
+          <div>
+            <h1>Avani's Portfolio</h1>
+          </div>
+          <div>
+            <Navbar
+              currentTab={currentTab}
+              setCurrentTab={setCurrentTab}
+            ></Navbar>
+          </div>
+        </header>
+      </section>
+    </div>
+  );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function Navbar(props) {
   const { currentTab, setCurrentTab } = props;

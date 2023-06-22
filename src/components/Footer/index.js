8616f21a460738828cfe0aab9ea017c45b23594c
Avani <avani.jadeja@gmail.com>
2023-06-21 21:36:04 -0500

    Addded footer logic in footer index.js file and added css for footer, install npm install react-icons --save react icon" " ;

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { VscGithubInverted } from "react-icons/vsc";
+import { AiFillLinkedin } from "react-icons/ai";
+import { BsStackOverflow } from "react-icons/bs";
 
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <ul>
+        <li>
+          <h2>Made by Avani ©</h2>
+        </li>
+
+        <a href="https://github.com/avanijadeja">
+          <li className="logo">
+            <VscGithubInverted />
+          </li>
+        </a>
+
+        <a href="https://www.linkedin.com/in/avaniba-jadeja-5b927a18/">
+          <li className="logo">
+            <AiFillLinkedin />
+          </li>
+        </a>
+
+        <a href="https://stackoverflow.com/users/22099285/avaniba-jadeja">
+          <li className="logo">
+            <BsStackOverflow />
+          </li>
+        </a>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;

8d570b6c57aab0e5b03950684eab043a7b4dadc1
Avani <avani.jadeja@gmail.com>
2023-06-24 11:16:26 -0500

    Added commands in Footer index.js file

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,28 +1,36 @@
+//  import react
 import React from "react";
+//  import react icons for footer
+//  import github icon
 import { VscGithubInverted } from "react-icons/vsc";
+// import linkedin icon
 import { AiFillLinkedin } from "react-icons/ai";
+// import stackoverflow icon
 import { BsStackOverflow } from "react-icons/bs";
 
 function Footer() {
   return (
+    // generated footer usinf <footer>
     <footer>
       <ul>
         <li>
           <h2>Made by Avani ©</h2>
         </li>
-
+        {/* Added link to github on github icon */}
         <a href="https://github.com/avanijadeja">
           <li className="logo">
             <VscGithubInverted />
           </li>
         </a>
 
+        {/*  Added link to linkedin on linkedin icon */}
         <a href="https://www.linkedin.com/in/avaniba-jadeja-5b927a18/">
           <li className="logo">
             <AiFillLinkedin />
           </li>
         </a>
 
+        {/* added link to stackoverflow on stackoverflow icon */}
         <a href="https://stackoverflow.com/users/22099285/avaniba-jadeja">
           <li className="logo">
             <BsStackOverflow />
@@ -33,4 +41,5 @@ function Footer() {
   );
 }
 
+// export footer function
 export default Footer;
